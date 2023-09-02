955ad321674201881172d8ed942764121ecef427
Am K <myawn@pm.me>
2023-09-02 22:56:50 +0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+[![status workflow test](https://github.com/cirebon-dev/Webftp/actions/workflows/php.yml/badge.svg)](https://github.com/cirebon-dev/Webftp/actions)
+
+This fork from unmaintained [Monsta-FTP](https://github.com/MonstaApps/Monsta-FTP) version 1 (GPL-3.0 license) to continued maintenance by cirebon-dev teams.
---
+++ languages/id_id.php
@@ -0,0 +1,146 @@
+<?php
+
+// Language: Indonesia(ID)
+// Author: guangrei
+// Last Updated: 22 Mei 2015
+
+$file_lang_name = "Indonesia (ID)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Sesi habis";
+$lang_title_errors = "Terjadi error";
+
+$lang_session_expired = "Sesi kamu sudah kadaluarsa";
+$lang_ip_conflict = "Alamat ip kamu konflik dengan sesi ip";
+
+$lang_btn_login = "Masuk";
+$lang_ftp_host = "FTP Host";
+$lang_port = "port";
+$lang_passive_mode = "Gaya pasif (default)";
+$lang_username = "Nama pengguna";
+$lang_password = "Password";
+$lang_ftp_ssl = "Menyambung dengan ssl (jika tersedia)";
+$lang_adv_interface = "Antarmuka modern";
+$lang_save_login = "Simpan login detail";
+$lang_language = "Bahasa";
+$lang_skin = "Kulit";
+$lang_ip_check = "Kunci sesi ke ip";
+
+$lang_skins_empty = "[skins] folder kosong!";
+$lang_skins_locked = "[skins] folder terkunci!";
+$lang_skins_missing = "[skins] folder hilang!";
+
+$lang_missing_fields = "Kesalahan! tolong lengkapi semua bidang yang ditandai";
+$lang_max_logins = "Kamu telah gagal login melebihi yang di tetapkan coba lagi dalam [n] menit";
+$lang_cant_connect = "Kesalahan! tidak dapat menyambung ke FTP host";
+$lang_cant_authenticate = "Kesalahan!tidak dapat memproses login kamu";
+$lang_ip_conflict = "Sebuah konflik ip terdeteksi";
+
+$lang_table_name = "Nama";
+$lang_table_size = "Ukuran";
+$lang_table_date = "Tanggal";
+$lang_table_time = "Waktu";
+$lang_table_user = "Pengguna";
+$lang_table_group = "Group";
+$lang_table_perms = "Akses";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "Segarkan";
+$lang_btn_cut = "Potong";
+$lang_btn_copy = "Salin";
+$lang_btn_paste = "Tempel";
+$lang_btn_rename = "Namai";
+$lang_btn_delete = "Hapus";
+$lang_btn_chmod = "Chmod";
+$lang_btn_logout = "Keluar";
+$lang_btn_save = "Simpan";
+$lang_btn_close = "Tutup";
+$lang_btn_cancel = "Batalkan";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Folder baru";
+$lang_btn_new_file = "File baru";
+$lang_btn_upload_file = "Unggah File";
+$lang_btn_upload_files = "Unggah Files";
+$lang_btn_upload_repeat = "unggah ulang";
+$lang_btn_upload_folder = "Unggah Folder";
+
+$lang_info_host = "Host";
+$lang_info_user = "Pengguna";
+$lang_info_upload_limit = "Batas upload";
+$lang_info_drag_drop = "Tarik & Letakkan";
+
+$lang_xfer_file = "File";
+$lang_xfer_size = "Ukuran";
+$lang_xfer_progress = "Proses";
+$lang_xfer_elapsed = "Waktu berlalu";
+$lang_xfer_uploaded = "Terupload";
+$lang_xfer_rate = "Nilai Transfer";
+$lang_xfer_remain = "Waktu Mengulang";
+
+$lang_move_conflict = "Tujuan folder adalah sebuah sub-folder dari asal folder";
+$lang_cant_rename = "Tidak dapat men namai [file]";
+$lang_cant_delete = "Tidak dapat menghapus [file]";
+
+$lang_folder_exists = "Folder [folder] sudah ada";
+$lang_folder_doesnt_exist = "Folder [folder] tidak ada";
+$lang_folder_cant_move = "Tidak dapat memindahkan [folder]";
+$lang_folder_cant_delete = "Tidak dapat menghapus [folder]";
+$lang_folder_cant_access = "Tidak dapat mengakses [folder]";
+$lang_folder_cant_make = "Tidak dapat menghapus [folder]";
+
+$lang_file_exists = "File [file] sudah ada";
+$lang_file_doesnt_exist = "File [file] tidak ada";
+$lang_file_cant_move = "Tidak dapat memindahkan [file]";
+$lang_file_cant_make = "Tidak dapat membuat [file]";
+
+$lang_server_error_down = "Server error mengunduh [file]";
+$lang_server_error_up = "Server error mengunggah [file]";
+$lang_browser_error_up = "Peramban error mengunggah [file]";
+$lang_file_size_error = "Ukuran file melebihi batas yang diperbolehkan";
+$lang_file_size_copy_error = "[file] melebihi batas yang diperbolehkan";
+
+$lang_folder_cant_chmod = "Tidak dapat men chmod [perms] [folder]";
+$lang_file_cant_chmod = "Tidak dapat men chmod [perms] [file]";
+$lang_chmod_max_777 = "Chmod tidak boleh lebih tinggi dari 777";
+$lang_chmod_owner = "Akses pengguna";
+$lang_chmod_group = "Akses group";
+$lang_chmod_public = "Akses publik";
+$lang_chmod_manual = "Manual";
+$lang_chmod_read = "Baca";
+$lang_chmod_write = "Tulis";
+$lang_chmod_exe = "Esekusi";
+$lang_chmod_no_support = "PHP server kamu tidak mendukung ftp_chmod";
+
+$lang_title_rename = "Menamai Files";
+$lang_title_chmod = "Ubah File Attribut";
+$lang_title_edit_file = "Sunting";
+$lang_title_new_file = "File Baru";
+$lang_title_new_folder = "Folder Baru";
+$lang_new_folder_name = "Masukan nama folder baru...";
+$lang_new_file_name = "Masukan nama file baru...";
+$lang_template = "Template";
+$lang_no_template = "Tidak ada Template";
+
+$lang_no_xmlhttp = "Peramban kamu tidak mendukung XMLHTTP!";
+$lang_support_drop = "Mendukung";
+$lang_no_support_drop = "Tidak mendukung";
+$lang_transfer_pending = "Pending";
+$lang_transferring_to_ftp = "Mentransfer ke server ftp...";
+$lang_no_file_selected = "Silahkan klik jelajah dan pilih sebuah file!";
+$lang_none_selected = "Silahkan pilih minimum satu file atau folder!";
+$lang_context_open = "Buka";
+$lang_context_download = "Unduh";
+$lang_context_edit = "Edit";
+$lang_context_cut = "Potong";
+$lang_context_copy = "Salin";
+$lang_context_paste = "Tempel";
+$lang_context_rename = "Namai";
+$lang_context_delete = "Hapus";
+$lang_context_chmod = "Chmod";
+
+?>
